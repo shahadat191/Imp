7c9f62ec0ee30adf9d8a36d339a4a448171320a6
shahadat191 <32817481+shahadat191@users.noreply.github.com>
2019-06-09 06:56:31 +0600

    Update dateQuery.cpp

--- dateQuery.cpp
+++ dateQuery.cpp
@@ -25,6 +25,17 @@ string findDayName(int d, int m, int y){
     string res =  dayName[tday];
     return res;
 }
+string findDayName(int d, int m, int y){
+    int prev = y - 1;
+    int tday = d + (prev * 365) + prev/4 - prev/100 + prev/400;
+    for(int i = 1; i < m; i++){
+        tday += monthDay[i];
+        if(i == 2 && isleap(y)) tday++;
+    }
+    tday = (tday - 1 + 7) % 7;
+    string res =  dayName[tday];
+    return res;
+}
 
 int main(){
     int d, m, y;
