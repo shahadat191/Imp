e6fb574c0f02b3e66b877ecc6c3d37af503b2d1b
shahadat191 <32817481+shahadat191@users.noreply.github.com>
2019-06-09 06:48:14 +0600

    Create dateQuery.cpp

---
+++ dateQuery.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool isleap(int year){
+    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+}
+int monthDay[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+string dayName[] = { "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY"};
+
+string findDayName(int d, int m, int y){
+    int tday = 0;
+    for(int i = 1; i < y; i++){
+        tday += isleap(i) ? 366 : 365;
+    }
+    for(int i = 1; i < m; i++){
+        tday += monthDay[i];
+        if(i == 2 && isleap(y)) tday++;
+    }
+    tday += d;
+    tday = (tday - 1 + 7) % 7;
+    string res =  dayName[tday];
+    return res;
+}
+
+int main(){
+    int d, m, y;
+    cin >> d >> m >> y;
+    string res = findDayName(d, m, y);
+    cout << res << '\n';
+}
